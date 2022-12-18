e4a5df2f9149062f16d3076b6ab307ebc04a2c26
Mark Smith <mark@qq.is>
2022-12-17 22:57:26 -0800

    day 17, sample passes, real fails

--- 2022/p17.ts
+++ 2022/p17.ts
@@ -5,7 +5,7 @@ import * as fs from "fs";
 
 const { program } = require("commander");
 
-program.option("-p1").option("-p2").option("-sample");
+program.option("-p1").option("-p2").option("-sample").option("-debug");
 program.parse();
 
 const options = program.opts();
@@ -13,16 +13,82 @@ const filename = "inputs/day17." + (options.Sample ? "sample" : "input");
 
 const lines = fs.readFileSync(filename, "utf8").split(/\r?\n/);
 
+const DEBUG = options.Debug;
+function dbg(...args: any) {
+  if (DEBUG) {
+    console.log(...args);
+  }
+}
+
 // PARSING LIZARDS HERE
 
-const shapes = class Whatever {
+class Point {
+  x: number = 0;
+  y: number = 0;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+const shapes = [
+  // Horizontal line
+  [new Point(0, 0), new Point(1, 0), new Point(2, 0), new Point(3, 0)],
+
+  // Plus
+  [
+    new Point(1, 0),
+    new Point(0, 1),
+    new Point(1, 1),
+    new Point(2, 1),
+    new Point(1, 2),
+  ],
+
+  // Right L
+  [
+    new Point(0, 0),
+    new Point(1, 0),
+    new Point(2, 0),
+    new Point(2, 1),
+    new Point(2, 2),
+  ],
+
+  // Vertical line
+  [new Point(0, 0), new Point(0, 1), new Point(0, 2), new Point(0, 3)],
+
+  // Square
+  [new Point(0, 0), new Point(1, 0), new Point(0, 1), new Point(1, 1)],
+];
+
+class Whatever {
   tiles: Array<number> = new Array(7).fill(0);
+
   moves: Array<number> = new Array();
+  movect: number = 0;
+
+  bounds(piece: Array<Point>): [Point, Point] {
+    var min = new Point(999, 999);
+    var max = new Point(0, 0);
+
+    piece.forEach((point) => {
+      min.x = Math.min(min.x, point.x);
+      min.y = Math.min(min.y, point.y);
+      max.x = Math.max(max.x, point.x);
+      max.y = Math.max(max.y, point.y);
+    });
+
+    return [min, max];
+  }
 
   highest(): number {
-    return Math.max(this.tiles);
+    return Math.max(...this.tiles);
   }
-};
+
+  nextMove(): number {
+    return this.moves[this.movect++ % this.moves.length];
+  }
+}
 
 function parse(lines: Array<string>): Whatever {
   var res = new Whatever();
@@ -38,7 +104,7 @@ function parse(lines: Array<string>): Whatever {
     });
   });
 
-  return new Whatever();
+  return res;
 }
 
 // DRAGONS HERE
@@ -46,7 +112,64 @@ function parse(lines: Array<string>): Whatever {
 function part1(lines: Array<string>): number {
   var input = parse(lines);
 
-  return 0;
+  var idx = 0;
+  for (var i = 0; i < 2022; i++) {
+    var piece = shapes[idx++ % shapes.length];
+    var [min, max] = input.bounds(piece);
+
+    var offset_x = 2;
+    var offset_y = input.highest() + 4;
+
+    var stopped = false;
+    while (!stopped) {
+      // Shift left/right if possible
+      var move = input.nextMove();
+      if (min.x + offset_x + move < 0 || max.x + offset_x + move >= 7) {
+        dbg("can't offset through walls", move);
+      } else {
+        // Determine if we're going to bang into another piece
+        var canMove = true;
+        piece.every((point) => {
+          if (input.tiles[point.x + offset_x + move] >= point.y + offset_y) {
+            dbg("can't offset through piece", move);
+            canMove = false;
+            return false;
+          }
+          return true;
+        });
+        if (canMove) {
+          dbg("offsetting", move);
+          offset_x += move;
+        }
+      }
+
+      // Now determine if we will collide
+      var canDrop = true;
+      piece.every((point) => {
+        var test = new Point(point.x + offset_x, point.y + offset_y);
+        if (input.tiles[test.x] >= test.y - 1) {
+          canDrop = false;
+          return false;
+        }
+        return true;
+      });
+      if (canDrop) {
+        dbg("dropping");
+        offset_y--;
+      } else {
+        piece.forEach((point) => {
+          input.tiles[point.x + offset_x] = Math.max(
+            input.tiles[point.x + offset_x],
+            point.y + offset_y
+          );
+        });
+        dbg("landed", input.tiles);
+        stopped = true;
+      }
+    }
+  }
+
+  return input.highest();
 }
 
 // HARDCORE DRAGONS HERE
