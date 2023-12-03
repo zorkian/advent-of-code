18258b8a000eaa6c09ed07937a38466f049ec1af
Mark Smith <mark@qq.is>
2023-12-02 19:54:20 -0800

    2015 day 1

---
+++ 2015/dayT.py
@@ -0,0 +1,49 @@
+import click
+import sys
+
+VERBOSE = False
+
+
+def log(*args, **kwargs):
+    if VERBOSE:
+        print(*args, **kwargs)
+
+
+@click.command()
+@click.option("-p1", is_flag=True, help="Run part 1.")
+@click.option("-p2", is_flag=True, help="Run part 2.")
+@click.option("-test", is_flag=True, help="Use test inputs.")
+@click.option("-v", is_flag=True, help="Print verbosely.")
+def main(p1, p2, test, v):
+    global VERBOSE
+    VERBOSE = v
+    log("p1 =", p1, "|| p2 =", p2, "|| test =", test, "|| v =", v)
+    inp = munge_input(load_input(test))
+    log("Munged input:", inp, "\n")
+    print(part1(inp) if p1 else part2(inp))
+
+
+def load_input(test):
+    prefix = "inputs/day" + sys.argv[0].split(".")[0][-1] + "."
+    with open(prefix + ("test" if test else "input")) as f:
+        return [l.strip() for l in f.readlines()]
+
+
+def munge_input(inp):
+    """
+    Convert the input lines into whatever structure we need to handle the
+    problem of the day.
+    """
+    return inp
+
+
+def part1(inp):
+    return 0
+
+
+def part2(inp):
+    return 0
+
+
+if __name__ == "__main__":
+    main()
