f45ae050fd6bd4c124405405201a202bb4e01c6a
Mark Smith <mark@qq.is>
2023-12-10 21:36:23 -0800

    switch to namedtuple for day10

--- 2023/day10.py
+++ 2023/day10.py
@@ -1,7 +1,11 @@
 import click
 import sys
 
+from collections import namedtuple
+
 VERBOSE = False
+
+Point = namedtuple("Point", ["x", "y"])
 
 
 class Pipe:
@@ -14,22 +18,22 @@ class Pipe:
         self.ctr = 0
 
     def inflate(self, pipes):
-        if type(self.p1) == tuple:
-            if self.p1[1] not in pipes:
+        if type(self.p1) == Point:
+            if self.p1.y not in pipes:
                 self.p1 = None
-            elif self.p1[0] not in pipes[self.p1[1]]:
+            elif self.p1.x not in pipes[self.p1.y]:
                 self.p1 = None
             else:
-                self.p1 = pipes[self.p1[1]][self.p1[0]]
+                self.p1 = pipes[self.p1.y][self.p1.x]
                 self.p1.adopt(self)
 
-        if type(self.p2) == tuple:
-            if self.p2[1] not in pipes:
+        if type(self.p2) == Point:
+            if self.p2.y not in pipes:
                 self.p2 = None
-            elif self.p2[0] not in pipes[self.p2[1]]:
+            elif self.p2.x not in pipes[self.p2.y]:
                 self.p2 = None
             else:
-                self.p2 = pipes[self.p2[1]][self.p2[0]]
+                self.p2 = pipes[self.p2.y][self.p2.x]
                 self.p2.adopt(self)
 
     def adopt(self, pipe):
@@ -40,20 +44,20 @@ class Pipe:
             else:
                 raise Exception("fuck2")
 
-        if type(self.p1) == tuple and self.p1 == (pipe.x, pipe.y):
+        if type(self.p1) == Point and self.p1 == (pipe.x, pipe.y):
             self.p1 = pipe
             self.ctr = pipe.ctr + 1
             return True
-        elif type(self.p2) == tuple and self.p2 == (pipe.x, pipe.y):
+        elif type(self.p2) == Point and self.p2 == (pipe.x, pipe.y):
             self.p2 = pipe
             self.ctr = pipe.ctr + 1
             return True
         return False
 
     def valid(self):
-        if type(self.p1) == tuple:
+        if type(self.p1) == Point:
             self.p1 = None
-        if type(self.p2) == tuple:
+        if type(self.p2) == Point:
             self.p2 = None
 
         if self.p1 is None or self.p2 is None:
@@ -89,20 +93,20 @@ class Pipe:
         while curPipe != self:
             coords = curPipe.nextLocation()
             # log(curPipe.x, curPipe.y, coords)
-            if not pipes[coords[1]][coords[0]].adopt(curPipe):
+            if not pipes[coords.y][coords.x].adopt(curPipe):
                 raise Exception("fuck", curPipe.x, curPipe.y)
             else:
-                if type(curPipe.p1) == tuple:
-                    curPipe.p1 = pipes[coords[1]][coords[0]]
-                elif type(curPipe.p2) == tuple:
-                    curPipe.p2 = pipes[coords[1]][coords[0]]
+                if type(curPipe.p1) == Point:
+                    curPipe.p1 = pipes[coords.y][coords.x]
+                elif type(curPipe.p2) == Point:
+                    curPipe.p2 = pipes[coords.y][coords.x]
 
-            curPipe = pipes[coords[1]][coords[0]]
+            curPipe = pipes[coords.y][coords.x]
 
     def nextLocation(self):
-        if type(self.p1) == tuple:
+        if type(self.p1) == Point:
             return self.p1
-        elif type(self.p2) == tuple:
+        elif type(self.p2) == Point:
             return self.p2
 
 
@@ -146,20 +150,20 @@ def munge_input(inp):
         x = 0
         for char in line:
             if char == "|":
-                rv[y][x] = Pipe(x, y, (x, y - 1), (x, y + 1))
+                rv[y][x] = Pipe(x, y, Point(x, y - 1), Point(x, y + 1))
             elif char == "-":
-                rv[y][x] = Pipe(x, y, (x - 1, y), (x + 1, y))
+                rv[y][x] = Pipe(x, y, Point(x - 1, y), Point(x + 1, y))
             elif char == "L":
-                rv[y][x] = Pipe(x, y, (x + 1, y), (x, y - 1))
+                rv[y][x] = Pipe(x, y, Point(x + 1, y), Point(x, y - 1))
             elif char == "J":
-                rv[y][x] = Pipe(x, y, (x - 1, y), (x, y - 1))
+                rv[y][x] = Pipe(x, y, Point(x - 1, y), Point(x, y - 1))
             elif char == "7":
-                rv[y][x] = Pipe(x, y, (x - 1, y), (x, y + 1))
+                rv[y][x] = Pipe(x, y, Point(x - 1, y), Point(x, y + 1))
             elif char == "F":
-                rv[y][x] = Pipe(x, y, (x + 1, y), (x, y + 1))
+                rv[y][x] = Pipe(x, y, Point(x + 1, y), Point(x, y + 1))
             elif char == "S":
                 rv[y][x] = Pipe(x, y, None, None, start=True)
-                start = (x, y)
+                start = Point(x, y)
             else:
                 rv[y][x] = None
             x += 1
@@ -167,7 +171,7 @@ def munge_input(inp):
     size = (x, y)
 
     # log(rv)
-    rv[start[1]][start[0]].beginAdoptions(rv)
+    rv[start.y][start.x].beginAdoptions(rv)
 
     # now remove everything that isn't wired up
     for y in rv.keys():
@@ -182,19 +186,19 @@ def munge_input(inp):
 def part1(inp):
     start, size, pipes = inp
 
-    pipe = pipes[start[1]][start[0]]
+    pipe = pipes[start.y][start.x]
     return int((max(pipe.p1.ctr, pipe.p2.ctr) + 1) / 2)
 
 
 def nextLoc(loc, size):
     # calculate next valid location
-    x = loc[0] + 1
-    if x < size[0]:
-        return (x, loc[1])
+    x = loc.x + 1
+    if x < size.x:
+        return (x, loc.y)
     x = 0
-    y = loc[1] + 1
-    if y < size[1]:
-        return (x, y)
+    y = loc.y + 1
+    if y < size.y:
+        return Point(x, y)
     return None
 
 
@@ -204,10 +208,10 @@ def inOrOut(loc, size, pipes):
     counts = []
 
     ctr = 0
-    for y in range(size[1]):
-        if pipes[y][loc[0]] and pipes[y][loc[0]].valid():
+    for y in range(size.y):
+        if pipes[y][loc.x] and pipes[y][loc.x].valid():
             ctr += 1
-        elif y == loc[1]:
+        elif y == loc.y:
             counts.append(ctr)
             ctr = 0
     counts.append(ctr)
@@ -217,10 +221,10 @@ def inOrOut(loc, size, pipes):
     wasOddV = True if counts[0] % 2 == 1 and counts[1] % 2 == 1 else False
 
     counts = []
-    for x in range(size[0]):
-        if pipes[loc[1]][x] and pipes[loc[1]][x].valid():
+    for x in range(size.x):
+        if pipes[loc.y][x] and pipes[loc.y][x].valid():
             ctr += 1
-        elif x == loc[0]:
+        elif x == loc.x:
             counts.append(ctr)
             ctr = 0
     counts.append(ctr)
@@ -238,25 +242,25 @@ def inOrOut(loc, size, pipes):
 
 def coordsFromLoc(loc):
     return [
-        (loc[0] - 1, loc[1] - 1),
-        (loc[0], loc[1] - 1),
-        (loc[0] + 1, loc[1] - 1),
-        (loc[0] - 1, loc[1]),
-        (loc[0] + 1, loc[1]),
-        (loc[0] - 1, loc[1] + 1),
-        (loc[0], loc[1] + 1),
-        (loc[0] + 1, loc[1] + 1),
+        Point(x=loc.x - 1, y=loc.y - 1),
+        Point(x=loc.x, y=loc.y - 1),
+        Point(x=loc.x + 1, y=loc.y - 1),
+        Point(x=loc.x - 1, y=loc.y),
+        Point(x=loc.x + 1, y=loc.y),
+        Point(x=loc.x - 1, y=loc.y + 1),
+        Point(x=loc.x, y=loc.y + 1),
+        Point(x=loc.x + 1, y=loc.y + 1),
     ]
 
 
 def paint(loc, size, tests, pipes):
     # if we've already determined, then skip
-    # if loc[1] in tests and loc[0] in tests[loc[1]]:
-    #    return (tests[loc[1]][loc[0]], nextLoc(loc, size))
+    # if loc.y in tests and loc[0] in tests[loc.y]:
+    #    return (tests[loc.y][loc[0]], nextLoc(loc, size))
 
     # if this location is a pipe, move on
-    if pipes[loc[1]][loc[0]]:
-        if pipes[loc[1]][loc[0]].valid():
+    if pipes[loc.y][loc.x]:
+        if pipes[loc.y][loc.x].valid():
             return (False, nextLoc(loc, size))
 
     # not a pipe, cast a ray and see if this is in or out
@@ -266,7 +270,7 @@ def paint(loc, size, tests, pipes):
     # return (ioo, nextLoc(loc, size))
 
     # initialize
-    tests[loc[1]][loc[0]] = ioo
+    tests[loc.y][loc.x] = ioo
 
     # flood fill
     tested = {}
@@ -277,14 +281,14 @@ def paint(loc, size, tests, pipes):
 
         if coord in tested:
             continue
-        if coord[0] < 0 or coord[1] < 0 or coord[0] >= size[0] or coord[1] >= size[1]:
+        if coord.x < 0 or coord.y < 0 or coord.x >= size.x or coord.y >= size.y:
             continue
-        if tests[coord[1]].get(coord[0], False):
+        if tests[coord.y].get(coord.x, False):
             continue
-        if pipes[coord[1]][coord[0]] and pipes[coord[1]][coord[0]].valid():
+        if pipes[coord.y][coord.x] and pipes[coord.y][coord.x].valid():
             continue
 
-        tests[coord[1]][coord[0]] = ioo
+        tests[coord.y][coord.x] = ioo
         tested[coord] = True
         coords += coordsFromLoc(coord)
 
@@ -300,20 +304,20 @@ def part2(inp):
     # are inside the loop. outside spaces are in zones that have an even count
     # of pipes on all sides.
     rv = 0
-    loc = (0, 0)
-    tests = {idx: {} for idx in range(size[1])}
+    loc = Point(x=0, y=0)
+    tests = {idx: {} for idx in range(size.y)}
     while loc is not None:
-        if loc[1] not in tests:
-            tests[loc[1]] = {}
+        if loc.y not in tests:
+            tests[loc.y] = {}
         ioo, loc2 = paint(loc, size, tests, pipes)
         if ioo:
             rv += 1
-        tests[loc[1]][loc[0]] = ioo
+        tests[loc.y][loc.x] = ioo
         loc = loc2
 
-    for y in range(size[1]):
+    for y in range(size.y):
         line = ""
-        for x in range(size[0]):
+        for x in range(size.x):
             if pipes[y][x] and pipes[y][x].valid:
                 line += "*"
             elif tests[y][x]:
