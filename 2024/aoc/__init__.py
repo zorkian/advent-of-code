eab33ea3f1e1747bfbfa5eea467d835491e62567
Mark Smith <mark@qq.is>
2024-12-02 21:15:12 -0800

    regexes forever

--- 2024/aoc/__init__.py
+++ 2024/aoc/__init__.py
@@ -1,6 +1,7 @@
 from .day import Day
 from .day1 import Day1
 from .day2 import Day2
+from .day3 import Day3
 
 
 def get_day(day_number, use_test_data=False) -> Day:
@@ -8,6 +9,8 @@ def get_day(day_number, use_test_data=False) -> Day:
         return Day1(use_test_data)
     elif day_number == 2:
         return Day2(use_test_data)
+    elif day_number == 3:
+        return Day3(use_test_data)
     raise Exception("Unknown day")
 
 
---
+++ 2024/aoc/day3.py
@@ -0,0 +1,38 @@
+from .day import Day
+
+
+class Day3(Day):
+    def __init__(self, use_test_data=False):
+        super().__init__(day_number=3, use_test_data=use_test_data)
+
+    def parse_input(self):
+        for line in self.input_data:
+            pass
+
+    def solve_part_one(self):
+        import re
+        total = 0
+        pattern = r'mul\((\d+),(\d+)\)'
+
+        for line in self.input_data:
+            matches = re.findall(pattern, line)
+            for x, y in matches:
+                total += int(x) * int(y)
+
+        return total
+
+    def solve_part_two(self):
+        import re
+        total = 0
+        pattern = r'mul\((\d+),(\d+)\)'
+
+        segments = ''.join(self.input_data).split("do()")
+        for segment in segments:
+            if "don't()" in segment:
+                segment = segment.split("don't()")[0]
+            print(segment)
+            matches = re.findall(pattern, segment)
+            for x, y in matches:
+                total += int(x) * int(y)
+
+        return total
