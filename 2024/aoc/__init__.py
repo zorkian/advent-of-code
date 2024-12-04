1958c3a4625e9548a7e3cad6932d9fe551c61dec
Mark Smith <mark@qq.is>
2024-12-03 20:43:45 -0800

    ready for day 4

--- 2024/aoc/__init__.py
+++ 2024/aoc/__init__.py
@@ -1,16 +1,18 @@
-from .day import Day
-from .day1 import Day1
-from .day2 import Day2
-from .day3 import Day3
+import importlib
 
+from .day import Day
+
+days = {}
+for i in range(1, 26):
+    try:
+        module = importlib.import_module(f".day{i}", package=__package__)
+        days[i] = getattr(module, f"Day{i}")
+    except (ImportError, AttributeError):
+        pass
 
 def get_day(day_number, use_test_data=False) -> Day:
-    if day_number == 1:
-        return Day1(use_test_data)
-    elif day_number == 2:
-        return Day2(use_test_data)
-    elif day_number == 3:
-        return Day3(use_test_data)
+    if day_number in days:
+        return days[day_number](use_test_data)
     raise Exception("Unknown day")
 
 
---
+++ 2024/aoc/day4.py
@@ -0,0 +1,16 @@
+from .day import Day
+
+
+class Day4(Day):
+    def __init__(self, use_test_data=False):
+        super().__init__(day_number=4, use_test_data=use_test_data)
+
+    def parse_input(self):
+        for line in self.input_data:
+            pass
+
+    def solve_part_one(self):
+        return 0
+
+    def solve_part_two(self):
+        return 0
